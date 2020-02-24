370266c5e8a7ac2b4e2c9c6dcc14b0a1884fcaa2
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-02-24 23:51:59 +0000

    Update contact form request endpoint

--- app/components/signup-form.js
+++ app/components/signup-form.js
@@ -21,14 +21,14 @@ export default class extends Component {
 			return;
 		}
 
-		await fetch('https://europe-west2-abcum-ltd.cloudfunctions.net/contact-surrealdb-com', {
+		await fetch('https://europe-west2-surreal-io.cloudfunctions.net/contact-surrealdb-com', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
 				email: this.email,
-			})
+			}),
 		});
 
 		this.completed = true;
