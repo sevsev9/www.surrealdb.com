ac7c230c62d802a0d0f39449c711f4abe39cbe20
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-10-22 12:28:41 +0100

    Fix typo in code example on roadmap page

--- app/snippets/roadmap/surrealql/expressions.sql
+++ app/snippets/roadmap/surrealql/expressions.sql
@@ -1,5 +1,5 @@
 -- Select a nested array, and filter based on an attribute
-SELECT emails[WHERE active = true] FROM person WHERE;
+SELECT emails[WHERE active = true] FROM person;
 
 -- Select all 1st, 2nd, and 3rd level people who this specific person record knows, or likes, as separate outputs
 SELECT ->knows->(? AS f1)->knows->(? AS f2)->(knows, likes AS e3 WHERE influencer = true)->(? AS f3) FROM person:tobie;
