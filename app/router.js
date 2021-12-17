f2e59ddbe6bb4ba3f64a6b47234e2c94e96d82a2
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-12-17 16:56:58 +0000

    Add logos and assets page

--- app/router.js
+++ app/router.js
@@ -12,6 +12,7 @@ Router.map(function() {
 	this.route('install');
 	this.route('roadmap');
 	this.route('hiring');
+	this.route('assets');
 	this.route('about');
 	this.route('blog');
 	this.route('docs');
