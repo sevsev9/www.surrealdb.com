d9e6c8085a37f08fa265baa5fb4529e6f1072b18
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-01-07 00:47:09 +0000

    Add sidebar navigation to features page

--- app/controllers/blog/post.js
+++ app/controllers/blog/post.js
@@ -2,7 +2,19 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { cache } from '@ascua/decorators';
 
+const slug = function(text) {
+	return String(text).replace(/[\s.]/gi, '-').replace(/[^a-zA-Z0-9_-]+/g, '').replace(/--/, '-').toLowerCase();
+}
+
 export default class extends Controller {
+
+	@cache get headings() {
+		return marked.lexer(this.html).filter(section => {
+			return section.type === 'heading' && section.depth === 2;
+		}).map(heading => {
+			return { text: heading.text, link: '#' + slug(heading.text) };
+		});
+	}
 
 	@cache get index() {
 		return this.posts.findIndex(post => post.id === this.model.id);
@@ -14,6 +26,10 @@ export default class extends Controller {
 
 	@cache get next() {
 		return this.posts[this.index-1] || null;
+	}
+
+	@cache get html() {
+		return this.model.html || '';
 	}
 
 }
