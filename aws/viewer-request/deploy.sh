a54cd6ad341e60471a9c275feec9db3f5a56739d
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-03-31 09:10:42 +0100

    Add default AWS profile

--- aws/viewer-request/deploy.sh
+++ aws/viewer-request/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
-aws cloudfront describe-function --name www-surrealdb-com-viewer-request > temp.json
+aws --profile surreal cloudfront describe-function --name www-surrealdb-com-viewer-request > temp.json
 ETAG=$(jq -r '.ETag' temp.json)
-aws cloudfront update-function --name www-surrealdb-com-viewer-request --function-code fileb://index.js --function-config Comment="",Runtime="cloudfront-js-1.0" --if-match $ETAG > temp.json
+aws --profile surreal cloudfront update-function --name www-surrealdb-com-viewer-request --function-code fileb://index.js --function-config Comment="",Runtime="cloudfront-js-1.0" --if-match $ETAG > temp.json
 ETAG=$(jq -r '.ETag' temp.json)
-aws cloudfront publish-function --name www-surrealdb-com-viewer-request --if-match $ETAG > temp.json
+aws --profile surreal cloudfront publish-function --name www-surrealdb-com-viewer-request --if-match $ETAG > temp.json
 rm -rf temp.json
