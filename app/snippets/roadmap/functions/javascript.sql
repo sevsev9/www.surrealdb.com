29b1cc3524525a4739a39588b380103f68f652c7
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-05-20 19:58:31 +0100

    Update javascript function example SQL

--- app/snippets/roadmap/functions/javascript.sql
+++ app/snippets/roadmap/functions/javascript.sql
@@ -3,11 +3,11 @@ CREATE film SET
 		{ rating: 6, user: user:bt8e39uh1ouhfm8ko8s0 },
 		{ rating: 8, user: user:bsilfhu88j04rgs0ga70 },
 	],
-	featured: fn::js -> function() {
+	featured: fn::script -> function() {
 		return this.ratings.filter(r => {
-			return r >= 7;
+			return r.rating >= 7;
 		}).map(r => {
-			return r * 10;
+			return { ...r, rating: r.rating * 10 };
 		});
 	},
 ;
