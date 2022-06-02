a0a3096e94b0b46db1e36fd601dd03037d325d3c
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-06-02 21:18:04 +0100

    Reduce blog image dimensions

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -1,10 +1,11 @@
 import { helper } from '@ember/component/helper';
 
-export function image([id]) {
-	if (localStorage.dev) {
-		return `https://cdn.brandsafe.io/w(1800)q(95)sharpen(1,0.4,0.8)cache(${new Date().getTime()})/${id}.jpg`;
-	} else {
+export function image([id, type]) {
+	switch (type) {
+	case 'main':
 		return `https://cdn.brandsafe.io/w(1800)q(95)sharpen(1,0.4,0.8)/${id}.jpg`;
+	default:
+		return `https://cdn.brandsafe.io/w(1600)q(95)sharpen(1,0.4,0.8)/${id}.jpg`;
 	}
 }
 
