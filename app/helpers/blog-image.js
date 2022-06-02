ad1d7e7b836e0530a7d24e973d739bbfae50dad2
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-06-02 17:13:31 +0100

    Improve compression size of blog images

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -1,7 +1,11 @@
 import { helper } from '@ember/component/helper';
 
 export function image([id]) {
-	return `https://cdn.brandsafe.io/w(1800)f(progressive)q(100)sharpen(1,0.4,0.8)/${id}.auto`;
+	if (localStorage.dev) {
+		return `https://cdn.brandsafe.io/w(1800)q(95)sharpen(1,0.4,0.8)cache(${new Date().getTime()})/${id}.jpg`;
+	} else {
+		return `https://cdn.brandsafe.io/w(1800)q(95)sharpen(1,0.4,0.8)/${id}.jpg`;
+	}
 }
 
 export default helper(image);
