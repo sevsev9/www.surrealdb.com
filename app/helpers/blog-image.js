68c430e536928df0ff9907f7ea228d642a28ee00
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-06-02 15:01:21 +0100

    Reduce file size of blog images

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function image([id]) {
-	return `https://cdn.brandsafe.io/w(1800)/${id}.png`;
+	return `https://cdn.brandsafe.io/w(1800)f(progressive)q(100)sharpen(1,0.4,0.8)/${id}.auto`;
 }
 
 export default helper(image);
