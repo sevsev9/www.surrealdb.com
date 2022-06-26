007f3ce68827653496893128ea6c7bcbf0ef8df8
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-06-26 19:11:35 +0100

    Fix javascript function example

--- app/snippets/roadmap/functions/javascript.sql
+++ app/snippets/roadmap/functions/javascript.sql
@@ -3,11 +3,11 @@ CREATE film SET
 		{ rating: 6, user: user:bt8e39uh1ouhfm8ko8s0 },
 		{ rating: 8, user: user:bsilfhu88j04rgs0ga70 },
 	],
-	featured: fn::script -> function() {
+	featured = fn::script -> function() {
 		return this.ratings.filter(r => {
 			return r.rating >= 7;
 		}).map(r => {
 			return { ...r, rating: r.rating * 10 };
 		});
-	},
+	}
 ;
