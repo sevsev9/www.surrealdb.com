804200fbb15326a32bfcd26e6b9250195a886f9e
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-07-24 08:04:09 +0100

    Update function() syntax for embedded scripts on v1.0.0-beta.3

--- app/initializers/prism.js
+++ app/initializers/prism.js
@@ -18,7 +18,7 @@ export default {
 				},
 				'variable': /[$][\w.]+/,
 				'cast': /[<](bool|int|float|string|number|decimal|datetime|duration|future)[>]/,
-				'function': /\b(?:count|(?:(array|crypto|geo|http|is|math|parse|rand|string|time|type)(::|\.)[\w]+)(::[\w]+)?)(?=\s*\()|(?:fn(::[\w]+)?\s*->( function\(\))?)/i,
+				'function': /\b(?:fn|function|count|(?:(array|crypto|fn|geo|http|is|math|parse|rand|string|time|type)::[\w]+)(::[\w]+)?)(?=\s*\()/i,
 				'keyword': /\b(?:ASC|AS|ASSERT|BEGIN( TRANSACTION)?|CANCEL( TRANSACTION)?|COLUMNS|COMMIT( TRANSACTION)?|CONTENT|CREATE|DATABASE|DB|DEFINE|DELETE|DESC|DESCRIBE|DIFF|DROP|ELSE|END|ES256|ES384|ES512|EVENT|FETCH|FIELD|FOR|FROM|GROUP( BY)?|HS256|HS384|HS512|IF|INDEX|INFO|INSERT(( IGNORE)? INTO)?|INTO|KILL|LET|LIMIT( BY)?|LIVE|LOGIN|MERGE|NAMESPACE|NS|ON DUPLICATE KEY UPDATE|ON|ORDER( BY)?|PASSHASH|PASSWORD|PERMISSIONS|PS256|PS384|PS512|RELATE|REMOVE|REPLACE|RETURN|RS256|RS384|RS512|SCHEMAFULL|SCHEMALESS|SCOPE|SELECT|SESSION|SET|SIGNIN|SIGNUP|SPLIT( ON)?|START( AT)?|TABLE|THEN|TIMEOUT|TOKEN|TYPE|UNIQUE|UPDATE|USE|VALUE|VALUES|VERSION|WHEN|WHERE)\b/,
 				'operator': /==|!=|\*=|\?=|=|!~|\*~|\?~|~|<=|<|>=|>|\+|-|\*|×|∙|\/|÷|∋|∌|∈|∉|⊇|⊃|⊅|⊆|⊂|⊄|&&|\|\||\b(?:AND|OR|IS NOT|IS|CONTAINSALL|CONTAINSANY|CONTAINSNONE|CONTAINSSOME|CONTAINSNOT|CONTAINS|ALLINSIDE|ANYINSIDE|NONEINSIDE|SOMEINSIDE|NOTINSIDE|INSIDE|OUTSIDE|INTERSECTS)\b/i,
 				'connector': /[.]|<->|<-|->/,
