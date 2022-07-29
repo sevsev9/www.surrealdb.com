e28774648718d9ae1412b8e710d9d8c61b4d98b5
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-07-29 16:35:56 +0100

    Ensure DEFINE SCOPE example is up-to-date

--- app/snippets/features/permissions/scope.sql
+++ app/snippets/features/permissions/scope.sql
@@ -1,8 +1,5 @@
 -- Enable scope authentication directly in SurrealDB
 DEFINE SCOPE account SESSION 24h
-	SIGNUP AS (CREATE user SET email=$email, pass=bcrypt.generate($pass))
-	SIGNIN AS (SELECT * FROM user WHERE email=$email AND bcrypt.compare(pass, $pass))
+	SIGNUP ( CREATE user SET email = $email, pass = crypto::argon2::generate($pass) )
+	SIGNIN ( SELECT * FROM user WHERE email = $email AND crypto::argon2::compare(pass, $pass) )
 ;
-
--- Enable authentication using a 3rd party provider
-DEFINE TOKEN auth0 ON SCOPE account TYPE HS512 VALUE "-----BEGIN PUBLIC KEY----- MIGfMA0G...";
