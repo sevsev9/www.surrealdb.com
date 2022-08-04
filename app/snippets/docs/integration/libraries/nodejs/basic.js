06d94e42fa81a8e8cd37b01f57d645facc75ba01
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-08-04 20:12:53 +0100

    Add initial client library documentation for Node.js

---
+++ app/snippets/docs/integration/libraries/nodejs/basic.js
@@ -0,0 +1,52 @@
+const Surreal = require('surrealdb.js');
+
+const db = new Surreal('http://127.0.0.1:8000/rpc');
+
+async function main() {
+
+	try {
+
+		// Signin as a namespace, database, or root user
+		await db.signin({
+			NS: 'test',
+			DB: 'test',
+			user: 'root',
+			pass: 'root',
+		});
+
+		// Select a specific namespace / database
+		await db.use('test', 'test');
+
+		// Create a new person with a random id
+		let created = await db.create("person", {
+			title: 'Founder & CEO',
+			name: {
+				first: 'Tobie',
+				last: 'Morgan Hitchcock',
+			},
+			marketing: true,
+			identifier: Math.random().toString(36).substr(2, 10),
+		});
+
+		// Update a person record with a specific id
+		let updated = await db.change("person:jaime", {
+			marketing: true,
+		});
+
+		// Select all people records
+		let people = await db.select("person");
+
+		// Perform a custom advanced query
+		let groups = await db.query('SELECT marketing, count() FROM type::table(tb) GROUP BY marketing', {
+			tb: 'person',
+		});
+
+	} catch (e) {
+
+		console.error('ERROR', e);
+
+	}
+
+}
+
+main();
