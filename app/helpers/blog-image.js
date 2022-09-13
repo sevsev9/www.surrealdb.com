81dd9fa47479e1d48a275c8fcd21c1ec6a9dff83
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-13 10:25:24 +0100

    Ensure blog images are not bigger than necessary

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -3,9 +3,15 @@ import { helper } from '@ember/component/helper';
 export function image([id, type]) {
 	switch (type) {
 	case 'main':
-		return `https://cdn.brandsafe.io/w(1800)q(95)sharpen(1,0.4,0.8)/${id}.jpg`;
+		return `https://cdn.brandsafe.io/w(1720)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
+	case 'post':
+		return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
+	case 'large':
+		return `https://cdn.brandsafe.io/w(1250)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
+	case 'small':
+		return `https://cdn.brandsafe.io/w(820)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
 	default:
-		return `https://cdn.brandsafe.io/w(1600)q(95)sharpen(1,0.4,0.8)/${id}.jpg`;
+		return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
 	}
 }
 
