66b9427c7c32c6454e3795685406a07e8a990400
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-30 22:16:42 +0100

    Add new v1.0.0-beta.8 release

--- app/controllers/releases.js
+++ app/controllers/releases.js
@@ -6,12 +6,84 @@ export default class extends Controller {
 	@cache get model() {
 		return [
 			{
+				time: new Date('2022-09-30T23:00:00'),
+				name: 'Release v1.0.0-beta.8',
+				path: 'section/releases/v1-0-0-beta-8',
+				link: 'v1.0.0-beta.8',
+				hash: 'v1-0-0-beta-8',
+				data: [
+					{
+						name: 'Features',
+						text: [
+							'Improve HTTP request error messages',
+							'Add support for dynamic expressions in Record IDs',
+							'Add support for <code>PERMISSIONS</code> clauses to be separated by commas or spaces',
+							'Allow deep-merging in <code>UPDATE ... MERGE</code> statements',
+							'Add debug and trace logging for authentication attempts',
+							'Make validation and parsing functions more robust with certain edge cases',
+							'Ignore empty or blank lines when using the SurrealDB SQL REPL',
+							'Use a dedicated executor thread for CPU-intensive functions',
+							'Ensure server listens to, and gracefully exits, on SIGINT/SIGTERM signals',
+							'Add <code>duration</code> functions for calculating durations as specific units',
+							'Add support for calculating the duration between two datetime values',
+							'Improve error message when automatically creating a table without authorization',
+							'Add support for uppercase and lowercase object keys in JWT authentication tokens',
+							'Allow namespaced claim aliases in JWT token using full domain-specific key names',
+							'Add support for retrieving JWT authentication token contents using <code>$token</code> parameter',
+							'Add support for different content return types on <code>/signup</code> and <code>/signin</code> HTTP routes',
+							'Add <code>session::token()</code> function for retrieving the contents of the JWT authentication token',
+							'Ensure <code>NONE</code> and <code>NULL</code> values are not automatically cast to another type when updating records',
+							'Add <code>/health</code> HTTP endpoint for checking the database and data storage engine status',
+							'Add <code>crypto::bcrypt::generate()</code> and <code>crypto::bcrypt::compare()</code> functions',
+							'Improve error messages for unique indexes, when inserting records which already exist in the index',
+							'Add <code>meta::tb()</code> and <code>meta::id()</code> functions for retrieving separate parts of a SurrealDB Record ID',
+							'Add support for using 3rd party authentication JWTs with <code>DEFINE TOKEN ... ON SCOPE ...</code>',
+						]
+					},
+					{
+						name: 'Bug fixes',
+						text: [
+							{ category: 'bug', text: 'Add support for escaped characters and unicode characters in strings' },
+							{ category: 'bug', text: 'Ensure datetimes work correctly in Eastern timezones' },
+							{ category: 'bug', text: 'Ensure <code>is::uuid()</code> parses valid UUIDs correctly' },
+							{ category: 'bug', text: 'Ensure <code>LET</code> statements throw errors correctly on failure' },
+							{ category: 'bug', text: 'Ensure Record IDs are parsed correctly when defined as a string' },
+							{ category: 'bug', text: 'Fix bug where escaped characters were not supported in regex values' },
+							{ category: 'bug', text: 'Ensure datetimes with milliseconds or nanoseconds are parsed correctly' },
+							{ category: 'bug', text: 'Ensure datetimes with partial timezones are correctly calculated' },
+							{ category: 'bug', text: 'Ensure <code>time::month()</code> returns the month of the specified datetime' },
+							{ category: 'bug', text: 'Ensure <code>FETCH</code> clauses fetch the respective data correctly' },
+							{ category: 'bug', text: 'Handle connection errors properly when WebSocket clients disconnect improperly' },
+							{ category: 'bug', text: 'Ensure HTTP session is not verified multiple times when requesting an invalid HTTP route' },
+							{ category: 'bug', text: 'Use <code>Accept</code> header instead of <code>Content-Type</code> header for client content negotiation' },
+							{ category: 'bug', text: 'Fix key scan range iteration in RocksDB, which caused SurrealDB to randomly crash' },
+							{ category: 'bug', text: 'Ensure authenticated session data is stored after successful scope signup / signin' },
+							{ category: 'bug', text: 'Fix bug where <code>http</code> functions would panic when an invalid URI was specified' },
+							{ category: 'bug', text: 'Ensure correct transaction type (optimistic / pessimistic) was initiated when using TiKV distributed storage engine' },
+							{ category: 'bug', text: 'Ensure <code>math::mean()</code>, <code>math::median()</code>, and <code>math::sqrt()</code> work correctly with empty or zero values' },
+							{ category: 'bug', text: 'Fix bug where <code>MultiPoint</code>, <code>MultiLine</code>, and <code>MultiPolygon</code> geometry values were not formatted correctly' },
+							{ category: 'bug', text: 'Fix bug where defined fields with empty values would be set on the root object, losing the object structure' },
+						]
+					},
+					{
+						name: 'Performance improvements',
+						text: [
+							{ category: 'performance', text: 'Miscellaneous performance optimizations and code cleanup' },
+							{ category: 'performance', text: 'Limit maximum allowed runtime and memory in JavaScript functions' },
+							{ category: 'performance', text: 'Ensure <code>crypto</code> and <code>rand</code> functions do not allow unbounded resource usage' },
+							{ category: 'performance', text: 'Ensure read-only transactions are used when write functionality is not needed when using TiKV distributed storage engine' },
+						]
+					},
+				]
+			},
+			{
 				time: new Date('2022-08-29'),
 				name: 'Release v1.0.0-beta.7',
 				path: 'section/releases/v1-0-0-beta-7',
 				link: 'v1.0.0-beta.7',
 				hash: 'v1-0-0-beta-7',
 				text: [
+					'Add a Windows <code>amd64</code> release build',
 					'Add support for Objects and Arrays as Record IDs',
 					'Add support for querying records using Record ID ranges',
 					'Add SQL <code>session</code> functions for retrieving session variables',
