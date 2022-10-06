7426c430938284296b5820a92d0a741d0bc35a0d
Jirapong Pansak <55589246+MaoMaoCake@users.noreply.github.com>
2022-10-06 11:21:20 +0700

    Ensure examples on features page are correct (#52)

--- app/snippets/features/permissions/scope.js
+++ app/snippets/features/permissions/scope.js
@@ -2,14 +2,14 @@
 let jwt = fetch('https://api.surrealdb.com/signup', {
 	method: 'POST',
 	headers: {
-		'Content-Type': 'application/json',
+		'Accept': 'application/json',
 		'NS': 'google', // Specify the namespace
 		'DB': 'gmail', // Specify the database
     },
 	body: JSON.stringify({
 		'NS': 'google',
 		'DB': 'gmail',
-		'SC': 'account'
+		'SC': 'account',
 		email: 'tobie@surrealdb.com',
 		pass: 'a85b19*1@jnta0$b&!'
 	}),
