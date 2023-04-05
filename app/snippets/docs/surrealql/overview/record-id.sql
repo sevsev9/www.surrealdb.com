9bfd7748982e8b4500a3c43130173d02de7b87a1
Emmanuel Keller <ekeller@open-search-server.com>
2023-04-05 11:39:22 +0100

    Add query results in overview (#79)

--- app/snippets/docs/surrealql/overview/record-id.sql
+++ app/snippets/docs/surrealql/overview/record-id.sql
@@ -1,1 +1,3 @@
+CREATE person:tobie SET name = 'Tobie';
+CREATE person:jaime SET name = 'Jaime';
 CREATE company:surrealdb SET name = 'SurrealDB', cofounders = [person:tobie, person:jaime];
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,7 @@ module.exports = function (defaults) {
 				'docker',
 				'toml',
 			],
+			copyToClipboard: true,
 		},
 		snippetPaths: ['app/snippets'],
 		snippetRegexes: {
