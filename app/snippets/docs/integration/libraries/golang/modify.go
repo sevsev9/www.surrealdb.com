6f783bcd5a08d006038d99c78527c23afdbf43ac
ElecTwix <51170038+ElecTwix@users.noreply.github.com>
2023-04-20 19:25:17 +0300

    Updated modify.go (#119)

--- app/snippets/docs/integration/libraries/golang/modify.go
+++ app/snippets/docs/integration/libraries/golang/modify.go
@@ -1,8 +1,8 @@
 // Update all records in a table
-db.Modify('person', map[string]interface{}{"op": "replace", "path": "/created_at", "value": time.Now()});
+db.Modify("person", []surrealdb.Patch{op: "replace", path: "/created_at", value: time.Now()})
 // Update a record with a specific ID
-db.Modify('person:tobie', map[string]interface{}{
-	{ "op": "replace", "path": "/settings/active", "value": false },
-	{ "op": "add", "path": "/tags", "value": []string{"developer", "engineer"} },
-	{ "op": "remove", "path": "/temp" },
-});
+db.Modify("person:tobie", []surrealdb.Patch{
+	{op: "replace", path: "/settings/active", value: false},
+	{op: "add", path: "/tags", value: []string{"developer", "engineer"}},
+	{op: "remove", path: "/temp"},
+})
--- app/snippets/docs/integration/libraries/golang/unmarshal_raw.go
+++ app/snippets/docs/integration/libraries/golang/unmarshal_raw.go
@@ -1,15 +1,15 @@
 // a simple user struct
 type testUser struct {
-	Username            string `json:"username,omitempty"`
-	Password            string `json:"password,omitempty"`
-	ID                  string `json:"id,omitempty"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	ID       string `json:"id,omitempty"`
 }
 
 username := "johnny"
 password := "123"
 
 // create test user with raw SurrealQL and unmarshal
-userData, err := s.db.Query("create users:johnny set Username = $user, Password = $pass", map[string]interface{}{
+userData, err := db.Query("create users:johnny set Username = $user, Password = $pass", map[string]interface{}{
 	"user": username,
 	"pass": password,
 })
