d04c8654514f6008c4e89caee0f5f8faa2ec961b
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-06-27 08:06:23 +0100

    Ensure gifs work on website

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -2,16 +2,16 @@ import { helper } from '@ember/component/helper';
 
 export function image([id, type]) {
 	switch (type) {
-	case 'main':
-		return `https://cdn.brandsafe.io/w(1720)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
-	case 'post':
-		return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
-	case 'large':
-		return `https://cdn.brandsafe.io/w(1250)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
-	case 'small':
-		return `https://cdn.brandsafe.io/w(820)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
-	default:
-		return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.jpg`;
+		case 'main':
+			return `https://cdn.brandsafe.io/w(1720)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+		case 'post':
+			return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+		case 'large':
+			return `https://cdn.brandsafe.io/w(1250)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+		case 'small':
+			return `https://cdn.brandsafe.io/w(820)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+		default:
+			return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
 	}
 }
 
