e1d39e705961bfe8b955881980788cae1d5c35a0
Micha de Vries <micha.de.vries@surrealdb.com>
2023-08-01 13:31:12 +0200

    Update Fly.io docs (#244)
    Co-authored-by: Naiyarah Hussain <naiyarah.h@gmail.com>

--- app/snippets/docs/deployment/fly.io/create-working-directory.sh
+++ app/snippets/docs/deployment/fly.io/create-working-directory.sh
@@ -1,2 +1,5 @@
 mkdir surrealdb-deployment && cd surrealdb-deployment
-nano Dockerfile
+## Uncomment the below line for macOS or Linux
+# nano Dockerfile
+## Uncomment the below line for Windows
+# notepad Dockerfile
